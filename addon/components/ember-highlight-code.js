f6846d1839d2bcbee817a0fc962b4f780001b29f
Chris Honniball <chonniball@gozooga.com>
2015-06-28 16:38:00 -0400

    Better line numbers

--- addon/components/ember-highlight-code.js
+++ addon/components/ember-highlight-code.js
@@ -1,59 +1,13 @@
 import Ember from 'ember';
 import layout from '../templates/components/ember-highlight-code';
 
-function str_pad(input, pad_length, pad_string, pad_type) {
-  //  discuss at: http://phpjs.org/functions/str_pad/
-  // original by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
-  // improved by: Michael White (http://getsprink.com)
-  //    input by: Marco van Oort
-  // bugfixed by: Brett Zamir (http://brett-zamir.me)
-  //   example 1: str_pad('Kevin van Zonneveld', 30, '-=', 'STR_PAD_LEFT');
-  //   returns 1: '-=-=-=-=-=-Kevin van Zonneveld'
-  //   example 2: str_pad('Kevin van Zonneveld', 30, '-', 'STR_PAD_BOTH');
-  //   returns 2: '------Kevin van Zonneveld-----'
-
-  var half = '',
-    pad_to_go;
-
-  var str_pad_repeater = function (s, len) {
-    var collect = '';
-
-    while (collect.length < len) {
-      collect += s;
-    }
-    collect = collect.substr(0, len);
-
-    return collect;
-  };
-
-  input += '';
-  pad_string = pad_string !== undefined ? pad_string : ' ';
-
-  if (pad_type !== 'STR_PAD_LEFT' && pad_type !== 'STR_PAD_RIGHT' && pad_type !== 'STR_PAD_BOTH') {
-    pad_type = 'STR_PAD_RIGHT';
-  }
-  if ((pad_to_go = pad_length - input.length) > 0) {
-    if (pad_type === 'STR_PAD_LEFT') {
-      input = str_pad_repeater(pad_string, pad_to_go) + input;
-    } else if (pad_type === 'STR_PAD_RIGHT') {
-      input = input + str_pad_repeater(pad_string, pad_to_go);
-    } else if (pad_type === 'STR_PAD_BOTH') {
-      half = str_pad_repeater(pad_string, Math.ceil(pad_to_go / 2));
-      input = half + input + half;
-      input = input.substr(0, pad_length);
-    }
-  }
-
-  return input;
-}
-
 export default Ember.Component.extend({
   layout: layout,
   
   tagName: 'pre',
   
   classNames: ["ember-highlight-code"],
-  classNameBindings: ["language"],
+  classNameBindings: ["language", "addLineNumbers:ember-hightlight-code-w-line-numbers"],
   
   
   ////////////////
@@ -61,42 +15,23 @@ export default Ember.Component.extend({
   ////////////////
   
   addLineNumbers: false,
+  startLineNumersAt: 1,
   
   ///////////////
   //! Computed //
   ///////////////
   
-  formattedCode: Ember.computed({
-    get: function(){
-      var that = this,
-      
-        // Fix indentation from the HTML source.
-        code = that.$().html(),
-        // Get the first line's spaces and replace an initial new line.
-        spaces = code.replace(/\n/, '').match(/^\s+/g),
-        // Replace the number of first line spaces on all lines following.
-        spacesRegExp = new RegExp('^' + spaces, 'gm'),
-        formattedCode = code.replace(spacesRegExp, '');
-      
-      // Remove empty line at end.
-      formattedCode = formattedCode.replace(/^\s+|\s+$/g, '');
-      
-      return formattedCode;
-    }
-  }),
-  
   formattedCodeWithLineNumbers: Ember.computed('formattedCode', {
     get: function(){
       var that = this,
-        formattedCode = that.get('formattedCode');
+        formattedCode = that.$().html();
       
       // Split the code into an array.
-      var codeArray = formattedCode.split('\n'),
-        lastElementNumberLength = String(codeArray.length - 1).length;
+      var codeArray = formattedCode.split('\n');
       
       // Loop through the array and add line numbers.
       codeArray.forEach(function(line, _i) {
-        codeArray[_i] = '<span class="line-number">' + str_pad((_i + 1), lastElementNumberLength, ' ', 'STR_PAD_LEFT') + "</span> " + line;
+        codeArray[_i] = '<span class="line-number">' + (_i + that.get('startLineNumersAt')) + "</span> " + line;
       });
       
       // Rejoin the array to be a code block.
@@ -113,12 +48,27 @@ export default Ember.Component.extend({
   
   didInsertElement: function() {
     var that = this,
-      code = (that.get('addLineNumbers')) ? that.get('formattedCodeWithLineNumbers') : that.get('formattedCode');
+       
+      // Fix indentation from the HTML source.
+      code = that.$().html(),
+      // Get the first line's spaces and replace an initial new line.
+      spaces = code.replace(/\n/, '').match(/^\s+/g),
+      // Replace the number of first line spaces on all lines following.
+      spacesRegExp = new RegExp('^' + spaces, 'gm'),
+      formattedCode = code.replace(spacesRegExp, '').replace(/^\s+|\s+$/g, '');
     
-    // Put formatted code into the element.
-    that.$().html(code);
+    that.$().html(formattedCode);
+    
+    that.set('rawCode', formattedCode);
     
     // Highlight the code.
     hljs.highlightBlock(that.$()[0]);
+    
+    if(that.get('addLineNumbers')) {
+      // Put formatted code into the element.
+      code = that.get('formattedCodeWithLineNumbers');
+      
+      that.$().html(code);
+    }
   }
 });
